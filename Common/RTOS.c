7d3d9fc7c2dc4690535d97738b60e6c267ee9083
Tobias Dubacher <t.dubacher@gmail.com>
2016-12-02 16:00:50 +0100

    add: LineFollowing

--- Common/RTOS.c
+++ Common/RTOS.c
@@ -29,32 +29,28 @@ static void AppTask(void* param) {
   }
 }
 
-static void LEDTask(void* param) {
-  const int *whichLED = (int*)param;
-
+static void KeyTask(void* param) {
   (void)param; /* avoid compiler warning */
-  for(;;) {
-    if (*whichLED==1) {
-      LED1_Neg();
-    } else if (*whichLED==2) {
-      LED2_Neg();
-    }
-    FRTOS1_vTaskDelay(pdMS_TO_TICKS(10));
 
-  }
+  for(;;) {
+#if PL_CONFIG_HAS_KEYS
+	#if PL_CONFIG_HAS_DEBOUNCE
+		 KEYDBNC_Process();
+	#else
+		KEY_Scan();
+	#endif
+#endif
+#if PL_CONFIG_HAS_EVENTS
+    EVNT_HandleEvent(APP_EventHandler, TRUE);
+#endif
+    FRTOS1_vTaskDelay(pdMS_TO_TICKS(10));
+  } /* FOR */
 }
 
 void RTOS_Init(void) {
-  static const int led1 = 1;
-  static const int led2 = 2;
-
-  EVNT_SetEvent(EVNT_STARTUP); /* set startup event */
-//  if (FRTOS1_xTaskCreate(AppTask, (signed portCHAR *)"App1", configMINIMAL_STACK_SIZE, (void*)&led1, tskIDLE_PRIORITY, NULL) != pdPASS) {
-//    for(;;){} /* error case only, stay here! */
-//  }
-//  if (FRTOS1_xTaskCreate(LEDTask, (signed portCHAR *)"Task1", configMINIMAL_STACK_SIZE, (void*)&led2, tskIDLE_PRIORITY, NULL) != pdPASS) {
-//      for(;;){} /* error case only, stay here! */
-//  }
+  if (FRTOS1_xTaskCreate(KeyTask, (signed portCHAR *)"Keys", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL) != pdPASS) {
+    for(;;){} /* error case only, stay here! */
+  }
 }
 
 void RTOS_Deinit(void) {
